c2850b0ed58dd9e603d54a01ac504f167ef13ce8
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-15 20:13:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,27 +3,27 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$text = $request_GET["text"];
+$text = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON Would you like to port your mobile number? \n";
+    $response  = "CON Hello! Would you like to port your mobile number? \n";
     $response .= "1. Yes \n";
     $response .= "2. No";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Please enter your SIM Card PIN;
-    
-} else if ($text == "1*1") {
-    // Business logic for first level response
-    $response = "*".$text;
+    $response = "CON Your are about to port your SIM car \n";
+    $response .= "1. Please enter your SIM PIN";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your porting request declined";
+    $response = "END Your port request has been declined";
 
+} else if($text == "1*1") { 
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your mobile number has been ported, please insert your new SIM car";
 }
 
 // Echo the response back to the API
