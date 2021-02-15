ae89d065d9db2b1d13e4b36323b80a72adfc5a60
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-15 19:19:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,87 +1,30 @@
 <?php
+// Read the variables sent via POST from AT gateway
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text = $_GET["text"];
 
-namespace App\Http\Controllers;
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON Would you like to port your phone number? \n";
+    $response .= "1. Yes \n";
+    $response .= "2. No";
 
-use Illuminate\Http\Request;
-
-class UssdController extends Controller
-{
-
-    public function onlineUssdMenu(Request $request)
-    {
-       $sessionId   = $request->get('sessionId');
-       $serviceCode = $request->get('serviceCode');
-       $phoneNumber = $request->get('phoneNumber');
-       $text        = $request->get('text');
-
-        // use explode to split the string text response from Africa's talking gateway into an array.
-
-        $ussd_string_exploded = explode("*", $text);
-
-        // Get ussd menu level number from the gateway
-
-        $level = count($ussd_string_exploded);
-
-        if ($text == "") {
-            // first response when a user dials our ussd code
-            $response  = "CON Welcome to Online Classes at HLAB \n";
-            $response .= "1. Register \n";
-            $response .= "2. About HLAB";
-        }
-
-        elseif ($text == "1") {
-            // when user respond with option one to register
-            $response = "CON Choose which framework to learn \n";
-            $response .= "1. Django Web Framework \n";
-            $response .= "2. Laravel Web Framework";
-        }
-
-        elseif ($text == "1*1") {
-            // when use response with option django
-            $response = "CON Please enter your first name";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 1 && $level == 3) {
-            $response = "CON Please enter your last name";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 1 && $level == 4) {
-            $response = "CON Please enter your email";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 1 && $level == 5) {
-            // save data in the database
-            $response = "END Your data has been captured successfully! Thank you for registering for Django online classes at HLAB.";
-        }
-
-        elseif ($text == "1*2") {
-            // when use response with option Laravel
-            $response = "CON Please enter your first name. ";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 2 && $level == 3) {
-            $response = "CON Please enter your last name";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 2 && $level == 4) {
-            $response = "CON Please enter your email";
-        }
-
-        elseif ($ussd_string_exploded[0] == 1 && $ussd_string_exploded[1] == 2 && $level == 5) {
-            // save data in the database
-
-            $response = "END Your data has been captured successfully! Thank you for registering for Laravel online classes at HLAB.";
-        }
-
-
-        elseif ($text == "2") {
-            // Our response a user respond with input 2 from our first level
-            $response = "END At HLAB we try to find a good balance between theory and practical!.";
-        }
-
-
-        // send your response back to the API
-        header('Content-type: text/plain');
-        echo $response;
-    }
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Please enter you SIM card PIN";
 }
+
+    $response = "END Your data has been captured successfully! Thank you for registering for Django online classes at HLAB.";
+}
+
+} else if($text == "2") { 
+        // This is a terminal request. Note how we start the response with END
+    $response = "END Your porting request has been declined.";
+    
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
+
+?>
