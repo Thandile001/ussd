f1c0cd49e84550ea826ae422fb2b75925fd50c56
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-17 20:02:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,24 +25,18 @@ if ($text == "") {
     $response = "CON DStv Installer \n";
     $response .= "1. Find nearby installer \n";
     $response .= "2. Verify Installer";
-    
- } else if ($text == "2*1") {
-    // Business logic for first level response
-    $response = "CON DStv Registered Installer. \n";
-    $response = "1.Find Nearby Installer. \n";
-    $response .= "2. Verify DStv Installer.";
-    
-} else if ($text == "2*1*1") {
+      
+} else if ($text == "2*1") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "End Thank you Mr Jwambi, a DStv accredited installer will contact you soon.";
  
-} else if ($text == "2*1*2") {
+} else if ($text == "2*2") {
     // Business logic for first level response
     $response = "CON DStv Installer verification. \n";
     $response .= "1. Please enter your Installer ID number.";
     
-} else if ($text == "2*1*2*1") {
+} else if ($text == "2*2*1") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "END Thank you Mr Jwambi, your accredited DStv Installer is Jabu Dlamini.";
