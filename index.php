3d106717586352f958e0cda56c9ad894f96a235d
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-17 16:44:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -146,23 +146,23 @@ if ($text == "") {
     // This is a terminal request. Note how we start the response with END
     $response = "END Unfortunately, the Gift Voucher number is invalid. Please try with another voucher number.";
     
-} else if ($text == "2") {
+} else if ($text == "2*1") {
     // Business logic for first level response
     $response = "CON DStv Registered Installer. \n";
     $response = "1.Find Nearby Installer. \n";
     $response .= "2. Verify DStv Installer.";
     
- } else if ($text == "2.1") {
+ } else if ($text == "2*1*1") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "END Thank you Mr Jwambi, a nearby installer will contact you shortly.";
     
- } else if ($text == "2.2") {
+ } else if ($text == "2*1*2") {
     // Business logic for first level response
     $response = "CON Verify DStv Installer. \n";
     $response .= "1. Please enter DStv Installer ID number."; 
     
- } else if ($text == "2*2*1") {
+ } else if ($text == "2*1*2*1") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "END Your are being served by Tshepo Mazibuko, an accredited and registered DStv Installer.";
