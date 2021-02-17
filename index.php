2129fd467f3466d96a2503dbe4585d44c4a019f2
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-17 11:27:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,29 +3,27 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$pinnumber = $GET["Pin"];
 $text = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON Hello! Would you like to port your mobile number? \n";
+    $response  = "CON Welcome Mr Thandile Jwambi, to Loxin Worx porting services. Would you like to port your mobile number? \n";
     $response .= "1. Yes \n";
     $response .= "2. No";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Your are about to port your SIM car \n";
-    $pinnumber .= "1. Please enter your SIM PIN";
-    
-} else if($text == "1*1") { 
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your mobile number has been ported, please insert your new SIM car";$pinnumber
+    $response = "CON Your are about to port your SIM card \n";
+    $response .= "1. Please enter your SIM card PIN";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your port request has been declined";
+    $response = "END Mr Jwambi, your port request has been declined.";
 
+} else if($text == "1*1") { 
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Thank Mr Jwambi, your mobile number has been ported succesfully. Please insert your new SIM card into your mobile phone.";
 }
 
 // Echo the response back to the API
