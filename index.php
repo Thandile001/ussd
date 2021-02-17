cadb7c74984c8a3f340ae2b989d1a5f81aff7df8
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-17 17:00:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ if ($text == "") {
     // This is a terminal request. Note how we start the response with END
     $response = "END Unfortunately, the Gift Voucher number is invalid. Please try with another voucher number.";
     
-} else if ($text == "2") {
+} if ($text == "2") {
     // Business logic for first level response
     $response = "CON DStv Registered Installer. \n";
     $response = "1.Find Nearby Installer. \n";
